dfc03618bf04e983d8072ac5be0f25bd3955d712
ankhuve <erik.c.forsberg@gmail.com>
2016-05-03 16:07:17 +0200

    Now play music on shuffle button click

--- app/dist/js/all.js
+++ app/dist/js/all.js
@@ -5288,30 +5288,59 @@ if (typeof CustomEvent !== 'function') {
 return SVG
 
 }));
-console.log("hej");
+var dice = document.getElementsByClassName("dice");
+var songs = [
+    '6ygVGWsgqv8xKlgb9coEXQ',
+    '64OcVQq3FBoGm2twUd3044',
+    '099TPpEaCSjEwysWZeTZv1',
+    '50KjvVYMfD3dik1Rkegkjz',
+    '3uDSPcjiNzcYaBhES4xrZS',
+    '5grmNnhOTNSbcq3R1heVT1'
+];
 
 document.getElementById("shuffleButton").addEventListener("click", shuffle);
 
+function createMusicObject(sourceURL){
+    return '<audio controls><source src="' + sourceURL + '" type="audio/mp3"></audio>'
+}
+
+function getTrack(trackID) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            var trackURL = JSON.parse(xhttp.responseText).preview_url;
+            var audioObj = document.getElementById("audioControl");
+            var audioSrc = document.getElementById("audioSource");
+            audioSrc.src = trackURL;
+            audioObj.load();
+            audioObj.play();
+            //createMusicObject(trackURL);
+            console.log(trackURL);
+        }
+    };
+    xhttp.open("GET", "https://api.spotify.com/v1/tracks/" + trackID, true);
+    xhttp.send();
+}
 
 function shuffle(){
-    randSide = Math.ceil(Math.random() * 6);
-    console.log(randSide);
-    dice = document.getElementsByClassName("dice");
+    resetDice();
+    var randSide = Math.ceil(Math.random() * 6);
+    var trackID = songs[randSide - 1];
+    getTrack(trackID);
     for(var i = 1; i <= dice.length; i++){
         if(i != randSide){
             var currentSide = document.getElementById("side" + (i));
-            if( ! currentSide.classList.contains("faded")){
-                currentSide.classList.add("faded");
-            }
+            currentSide.classList.add("faded");
 
         } else{
-            if( ! dice[i].classList.contains("active")){
-                dice[i].classList.add("active");
+            dice[i - 1].classList.add("active");
 
-            }
         }
-
-        console.log("ifew");
     }
 }
 
+function resetDice(){
+    for(var i = 0; i < dice.length; i++){
+        dice[i].className = "dice";
+    }
+}
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,42 @@
-console.log("hej");
-
 var dice = document.getElementsByClassName("dice");
+var songs = [
+    '6ygVGWsgqv8xKlgb9coEXQ',
+    '64OcVQq3FBoGm2twUd3044',
+    '099TPpEaCSjEwysWZeTZv1',
+    '50KjvVYMfD3dik1Rkegkjz',
+    '3uDSPcjiNzcYaBhES4xrZS',
+    '5grmNnhOTNSbcq3R1heVT1'
+];
+
 document.getElementById("shuffleButton").addEventListener("click", shuffle);
 
+function createMusicObject(sourceURL){
+    return '<audio controls><source src="' + sourceURL + '" type="audio/mp3"></audio>'
+}
+
+function getTrack(trackID) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            var trackURL = JSON.parse(xhttp.responseText).preview_url;
+            var audioObj = document.getElementById("audioControl");
+            var audioSrc = document.getElementById("audioSource");
+            audioSrc.src = trackURL;
+            audioObj.load();
+            audioObj.play();
+            //createMusicObject(trackURL);
+            console.log(trackURL);
+        }
+    };
+    xhttp.open("GET", "https://api.spotify.com/v1/tracks/" + trackID, true);
+    xhttp.send();
+}
 
 function shuffle(){
     resetDice();
     var randSide = Math.ceil(Math.random() * 6);
-    console.log(randSide);
+    var trackID = songs[randSide - 1];
+    getTrack(trackID);
     for(var i = 1; i <= dice.length; i++){
         if(i != randSide){
             var currentSide = document.getElementById("side" + (i));
@@ -15,15 +44,13 @@ function shuffle(){
 
         } else{
             dice[i - 1].classList.add("active");
-        }
 
-        console.log("ifew");
+        }
     }
 }
 
 function resetDice(){
     for(var i = 0; i < dice.length; i++){
-        console.log(i);
         dice[i].className = "dice";
     }
 }
